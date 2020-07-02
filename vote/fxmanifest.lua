568a990f4df848e4e284a347a28a87a3c333c3b6
cibor <paul.meyer.vithu@gmail.com>
2020-07-02 18:15:57 +0200

    add config.lua and add offline players management

--- vote/fxmanifest.lua
+++ vote/fxmanifest.lua
@@ -2,4 +2,8 @@ description 'Vote plugin pour Top-Serveurs (top-serveurs.net)'
 
 server_script "lib/VotePlugin.net.dll"
 
-server_script "example.lua"
+server_script {
+    "config.lua",
+    "example.lua",
+    "example_esx.lua"
+}
